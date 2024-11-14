2ae59950c249258da5297e90e41d0a348ccc57f4
Prince mali <maliprince909@gmail.com>
2024-11-14 22:11:29 +0530

    .

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ main().then((res) => {
 }).catch((err) => {console.log(err)});
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/bharatFootwear');
+    await mongoose.connect(`${process.env.DB_URL}`);
 };
 
 // middelwares
